7f92c08c590af61850efe18481231cca3f67c705
tasneemkhaled <tasneemkhaled350@gmail.com>
2024-08-28 20:34:47 +0200

    commit1

--- lib/models/tune_model.dart
+++ lib/models/tune_model.dart
@@ -1,11 +1,11 @@
 import 'dart:ui';
 
-class TuneModel{
- final Color color;
+class TuneModel {
+  final Color color;
   final String sound;
-   const TuneModel({required this.color,required this.sound});
-  void playSound(){
-  final player =AudioPlayer();
-  player.play(AssetSourse())
+  const TuneModel({required this.color, required this.sound});
+  void playSound() {
+    final player = AudioPlayer();
+    player.play(AssetSourse());
   }
 }
